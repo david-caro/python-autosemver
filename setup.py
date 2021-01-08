3f7faea6e33c94c54801ab79bb1d15d28373a947
danielzs <raket_83@yahoo.se>
2021-01-08 13:37:57 +0100

    [setup] Add python version info tags for pypi
    These are tags for searching and browsing on PyPi
    
    Sem-Ver: bugfix

--- setup.py
+++ setup.py
@@ -80,4 +80,13 @@ if __name__ == "__main__":
                 "bugtracker_url=autosemver:distutils",
             ],
         },
+        classifiers=[
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+        ],
     )
