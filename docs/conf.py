d310b3500115a241e666360fd525fb8103bbff87
David Caro <david@dcaro.es>
2022-07-09 11:37:43 +0200

    docs:Use "en" as language

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
-    "sphinxcontrib.napoleon",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -100,7 +100,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
